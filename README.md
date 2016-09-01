50a07182272be9d38aa6cd03b9a8a1fd9360d2e4
Xaver Kapeller <xaver.kapeller@gmail.com>
2016-09-01 20:40:27 +0200

    Added UI test. Minor readme update.

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 An example app using a `SearchView` to filter items in a `RecyclerView` while taking full advantage of item animations!
 
- - **Backwards compatible all the way down to Froyo (Android 2.2, API leve 7)!**
+ - **Backwards compatible all the way down to Froyo (Android 2.2, API leve 8)!**
  - **Item Animations are supported starting with Honeycomb (Android 3.0, API level 11)!**
+ 
+[![Build Status](https://travis-ci.org/Wrdlbrnft/Searchable-RecyclerView-Demo.svg?branch=master)](https://travis-ci.org/Wrdlbrnft/Searchable-RecyclerView-Demo)
 
-You can find a complete walkthrough of how to implement this [**on Stack Overflow**](http://stackoverflow.com/a/30429439/2310866).
+You can find a walkthrough of how to implement an app with these features [**on Stack Overflow**](http://stackoverflow.com/a/30429439/2310866).
 
 Or if you want to try out the app first you can download it from the Play Store:
 
@@ -13,4 +15,4 @@ Or if you want to try out the app first you can download it from the Play Store:
 
 ## **For more information visit the [project homepage](https://wrdlbrnft.github.io/Searchable-RecyclerView-Demo/)**
 
-![Demo Image](http://i.stack.imgur.com/qDtH7.gif)
+![Demo Image](http://i.stack.imgur.co§m/qDtH7.gif)
---
+++ app/src/androidTest/java/com/github/wrdlbrnft/searchablerecyclerviewdemo/ui/activities/MainActivityTest.java
@@ -0,0 +1,96 @@
+package com.github.wrdlbrnft.searchablerecyclerviewdemo.ui.activities;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.github.wrdlbrnft.searchablerecyclerviewdemo.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction appCompatImageView = onView(
+                allOf(withId(R.id.search_button), withContentDescription("Search"),
+                        withParent(allOf(withId(R.id.search_bar),
+                                withParent(withId(R.id.action_search)))),
+                        isDisplayed()));
+        appCompatImageView.perform(click());
+
+        ViewInteraction searchAutoComplete = onView(
+                allOf(withId(R.id.search_src_text),
+                        withParent(allOf(withId(R.id.search_plate),
+                                withParent(withId(R.id.search_edit_frame)))),
+                        isDisplayed()));
+        searchAutoComplete.perform(replaceText("lac"), closeSoftKeyboard());
+
+        ViewInteraction textView = onView(
+                allOf(withText("Blackhat"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.recycler_view),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Blackhat")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withText("The Woman in Black: Angel of Death"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.recycler_view),
+                                        1),
+                                0),
+                        isDisplayed()));
+        textView2.check(matches(withText("The Woman in Black: Angel of Death")));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
